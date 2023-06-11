1570515700e9b76302f637ba9a3699a8007f182f
Tomiwa <63702479+Tomiwa-S@users.noreply.github.com>
2023-06-12 00:14:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var mq2 = document.getElementById("mq2");
 var mq7 = document.getElementById("mq7");
 var mq135 = document.getElementById("mq135");
+var sent = false;
+var threshold = 3;
 async function getThingSpeakData() {
     const url = "https://api.thingspeak.com/channels/2077841/feeds.json";
     const post = await fetch(url).then((res) => res.json());
@@ -9,12 +11,16 @@ async function getThingSpeakData() {
     looper(post["feeds"],field1,"field1");
     looper(post["feeds"],field2,"field2");
     looper(post["feeds"],field3,"field3");
-    const mean = arr =>(arr.reduce((a,b)=>parseFloat(a)+parseFloat(b))/arr.length).toFixed(2);
-    mq2.innerHTML = mean(field1);
-    mq7.innerHTML = mean(field2);
-    mq135.innerHTML = mean(field3);
+    //const mean = arr =>(arr.reduce((a,b)=>parseFloat(a)+parseFloat(b))/arr.length).toFixed(2);
+    mq2.innerHTML = field1[99];
+    mq7.innerHTML = field2[99];
+    mq135.innerHTML = field3[99];
+    if(field1[99]>=threshold&&!sent){
+    Threshold();
+    };
 };
 setInterval(getThingSpeakData,500);
+function Threshold(){
 Email.send({
     Host : "smtp.elasticemail.com",
     Username : "timmeysam@gmail.com",
@@ -22,8 +28,10 @@ Email.send({
     To : 'ermasterde@gmail.com',
     From : "timmeysam@gmail.com",
     Subject : "ALERT- Vehicle Pollution Threshold exceeded",
-    Body : "Hi Tomiwa \n You should consider having your vehicle checked as it voiolates the FRSC pollution limit.\n This is a warning. Your data would soon be sent to the FRSC for prosecution if no action is taken to mitigate this."
+    Body : "Hi Tomiwa,"+ "\n" "You should consider having your vehicle checked as it voiolates the FRSC pollution limit.\n This is a warning. Your data would soon be sent to the FRSC for prosecution if no action is taken to mitigate this."
   }).then(
   message => alert(message)
   );
+    sent =true;
+};
 
