9f9812fd69c8a061a8836a35982e81fb0d8dac09
Tomiwa <63702479+Tomiwa-S@users.noreply.github.com>
2023-06-12 00:55:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.cookie = "cookieName=value; SameSite=None; Secure";
 var mq2 = document.getElementById("mq2");
 var mq7 = document.getElementById("mq7");
 var mq135 = document.getElementById("mq135");
@@ -11,28 +12,25 @@ async function getThingSpeakData() {
     looper(post["feeds"],field1,"field1");
     looper(post["feeds"],field2,"field2");
     looper(post["feeds"],field3,"field3");
-    //const mean = arr =>(arr.reduce((a,b)=>parseFloat(a)+parseFloat(b))/arr.length).toFixed(2);
     mq2.innerHTML = field1[99];
     mq7.innerHTML = field2[99];
     mq135.innerHTML = field3[99];
     if(field1[99]>=threshold&&!sent){
-    Threshold();
-     alert("Threshold Exceeded");
+        Threshold();
     };
 };
 setInterval(getThingSpeakData,500);
 function Threshold(){
-Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "timmeysam@gmail.com",
-    Password : "C009E876011C08AF3AB0E2993A272DCAF312",
-    To : 'ermasterde@gmail.com',
-    From : "timmeysam@gmail.com",
-    Subject : "ALERT- Vehicle Pollution Threshold exceeded",
-    Body : "Hi Tomiwa,"+ "\n" "You should consider having your vehicle checked as it voiolates the FRSC pollution limit.\n This is a warning. Your data would soon be sent to the FRSC for prosecution if no action is taken to mitigate this."
-  }).then(
-  message => alert(message)
-  );
-    sent =true;
-};
-
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "timmeysam@gmail.com",
+        Password : "C009E876011C08AF3AB0E2993A272DCAF312",
+        To : 'ermasterde@gmail.com',
+        From : "timmeysam@gmail.com",
+        Subject : "ALERT- Vehicle Pollution Threshold exceeded",
+        Body : "Hi Tomiwa,"+ "\n" +"You should consider having your vehicle checked as it voiolates the FRSC pollution limit.\n This is a warning. Your data would soon be sent to the FRSC for prosecution if no action is taken to mitigate this."
+      }).then(
+      message => alert("limit exceeded")
+      );
+        sent =true;
+    };
